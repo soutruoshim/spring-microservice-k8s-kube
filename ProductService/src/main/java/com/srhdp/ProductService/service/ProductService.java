c42b5f33831dc3f103d129cf3f99a65179ef33dc
soutruoshim <sout.rahim@gmail.com>
2023-07-17 22:01:32 +0700

    config application.yaml and post post product

---
+++ ProductService/src/main/java/com/srhdp/ProductService/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.srhdp.ProductService.service;
+
+import com.srhdp.ProductService.model.ProductRequest;
+
+public interface ProductService {
+    long addProduct(ProductRequest productRequest);
+}
