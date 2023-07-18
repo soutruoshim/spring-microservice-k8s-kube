cb22439222e95b42a2bb251bf6f4e5a35187b9b3
soutruoshim <sout.rahim@gmail.com>
2023-07-18 17:49:25 +0700

    create order

--- OrderService/src/main/java/com/srhdp/OrderService/service/OrderService.java
+++ OrderService/src/main/java/com/srhdp/OrderService/service/OrderService.java
@@ -1,4 +1,7 @@
 package com.srhdp.OrderService.service;
 
+import com.srhdp.OrderService.model.OrderRequest;
+
 public interface OrderService {
+    long placeOrder(OrderRequest orderRequest);
 }
--- OrderService/src/main/java/com/srhdp/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/srhdp/OrderService/service/OrderServiceImpl.java
@@ -1,9 +1,38 @@
 package com.srhdp.OrderService.service;
 
+import com.srhdp.OrderService.entity.Order;
+import com.srhdp.OrderService.model.OrderRequest;
+import com.srhdp.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @Log4j2
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+    @Override
+    public long placeOrder(OrderRequest orderRequest) {
+        //Order Entity -> Save the data with Status Order Created
+        //Product Service - Block Products (Reduce the Quantity)
+        //Payment Service -> Payments -> Success-> COMPLETE, Else
+        //CANCELLED
+
+        log.info("Placing Order Request: {}", orderRequest);
+        log.info("Creating Order with Status CREATED");
+        Order order = Order.builder()
+                .amount(orderRequest.getTotalAmount())
+                .orderStatus("CREATED")
+                .productId(orderRequest.getProductId())
+                .orderDate(Instant.now())
+                .quantity(orderRequest.getQuantity())
+                .build();
+        order = orderRepository.save(order);
+        log.info("Order Places successfully with Order Id: {}", order.getId());
+        return order.getId();
+
+    }
 }
