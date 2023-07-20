0d5ab819ea9b8cf38e81ebe858099d329dcf45c6
soutruoshim <sout.rahim@gmail.com>
2023-07-20 08:41:36 +0700

    fign client order payment

--- OrderService/src/main/java/com/srhdp/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/srhdp/OrderService/service/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.srhdp.OrderService.service;
 
 import com.srhdp.OrderService.entity.Order;
+import com.srhdp.OrderService.external.client.PaymentService;
 import com.srhdp.OrderService.external.client.ProductService;
+import com.srhdp.OrderService.external.request.PaymentRequest;
 import com.srhdp.OrderService.model.OrderRequest;
 import com.srhdp.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -17,6 +19,8 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
     @Autowired
     private ProductService productService;
+    @Autowired
+    private PaymentService paymentService;
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         //Order Entity -> Save the data with Status Order Created
@@ -35,6 +39,27 @@ public class OrderServiceImpl implements OrderService {
                 .orderDate(Instant.now())
                 .quantity(orderRequest.getQuantity())
                 .build();
+
+        log.info("Calling Payment Service to complete the payment"+orderRequest.getPaymentMode());
+        PaymentRequest paymentRequest
+                = PaymentRequest.builder()
+                .orderId(order.getId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .amount(orderRequest.getTotalAmount())
+                .build();
+
+        String orderStatus = null;
+
+        try {
+            paymentService.doPayment(paymentRequest);
+            log.info("Payment done Successfully. Changing the Oder status to PLACED");
+            orderStatus = "PLACED";
+        } catch (Exception e) {
+            log.error("Error occurred in payment. Changing order status to PAYMENT_FAILED");
+            orderStatus = "PAYMENT_FAILED";
+        }
+
+        order.setOrderStatus(orderStatus);
         order = orderRepository.save(order);
         log.info("Order Places successfully with Order Id: {}", order.getId());
         return order.getId();
