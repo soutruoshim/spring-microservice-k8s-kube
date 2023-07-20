4398b12f2388f748b7df831d4fac991922aa8e4d
soutruoshim <sout.rahim@gmail.com>
2023-07-20 07:49:40 +0700

    init paymentservice

--- OrderService/src/main/java/com/srhdp/OrderService/config/FeignConfig.java
+++ OrderService/src/main/java/com/srhdp/OrderService/config/FeignConfig.java
@@ -1,2 +1,15 @@
-package com.srhdp.OrderService.config;public class FeignConfig {
+package com.srhdp.OrderService.config;
+
+import com.srhdp.OrderService.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    ErrorDecoder errorDecoder() {
+        return new CustomErrorDecoder();
+    }
 }
--- OrderService/src/main/java/com/srhdp/OrderService/external/response/ErrorResponse.java
+++ OrderService/src/main/java/com/srhdp/OrderService/external/response/ErrorResponse.java
@@ -1,2 +1,14 @@
-package com.srhdp.OrderService.external.response;public class ErrorResponse {
+package com.srhdp.OrderService.external.response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String errorMessage;
+    private String errorCode;
 }
