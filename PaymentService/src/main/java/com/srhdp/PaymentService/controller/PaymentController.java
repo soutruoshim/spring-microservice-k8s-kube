1ef61e232e7b5a55bd6db1af4cc8529b4ff847c1
soutruoshim <sout.rahim@gmail.com>
2023-07-20 18:48:37 +0700

    get more order detail with product and payment

--- PaymentService/src/main/java/com/srhdp/PaymentService/controller/PaymentController.java
+++ PaymentService/src/main/java/com/srhdp/PaymentService/controller/PaymentController.java
@@ -1,14 +1,12 @@
 package com.srhdp.PaymentService.controller;
 
 import com.srhdp.PaymentService.model.PaymentRequest;
+import com.srhdp.PaymentService.model.PaymentResponse;
 import com.srhdp.PaymentService.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("payment")
@@ -20,6 +18,13 @@ public class PaymentController {
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
         return new ResponseEntity<>(
                 paymentService.doPayment(paymentRequest),
+                HttpStatus.OK
+        );
+    }
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
+        return new ResponseEntity<>(
+                paymentService.getPaymentDetailsByOrderId(orderId),
                 HttpStatus.OK
         );
     }
--- PaymentService/src/main/java/com/srhdp/PaymentService/service/PaymentServiceImpl.java
+++ PaymentService/src/main/java/com/srhdp/PaymentService/service/PaymentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.srhdp.PaymentService.service;
 
 import com.srhdp.PaymentService.entity.TransactionDetails;
+import com.srhdp.PaymentService.model.PaymentMode;
 import com.srhdp.PaymentService.model.PaymentRequest;
+import com.srhdp.PaymentService.model.PaymentResponse;
 import com.srhdp.PaymentService.repository.TransactionDetailsRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +36,24 @@ public class PaymentServiceImpl implements PaymentService{
         log.info("Transaction Completed with Id: {}", transactionDetails.getId());
 
         return transactionDetails.getId();
+    }
+    @Override
+    public PaymentResponse getPaymentDetailsByOrderId(String orderId) {
+        log.info("Getting payment details for the Order Id: {}", orderId);
+
+        TransactionDetails transactionDetails
+                = transactionDetailsRepository.findByOrderId(Long.valueOf(orderId));
+
+        PaymentResponse paymentResponse
+                = PaymentResponse.builder()
+                .paymentId(transactionDetails.getId())
+                .paymentMode(PaymentMode.valueOf(transactionDetails.getPaymentMode()))
+                .paymentDate(transactionDetails.getPaymentDate())
+                .orderId(transactionDetails.getOrderId())
+                .status(transactionDetails.getPaymentStatus())
+                .amount(transactionDetails.getAmount())
+                .build();
+
+        return paymentResponse;
     }
 }
